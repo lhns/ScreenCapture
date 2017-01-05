570e08f4db6d95db23013ed8d272c905a43241a0
Pierre Kisters <pierrekisters@gmail.com>
2017-01-05 22:44:49 +0100

    big performance improvements

--- src/main/scala/org/lolhens/screencapture/CaptureReceiver.scala
+++ src/main/scala/org/lolhens/screencapture/CaptureReceiver.scala
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress
 import javax.swing.JFrame
 
 import akka.actor.ActorSystem
+import swave.core.Buffer.OverflowStrategy
 import swave.core.StreamEnv
 
 /**
@@ -13,7 +14,9 @@ import swave.core.StreamEnv
 object CaptureReceiver {
   def apply(graphicsDevice: GraphicsDevice, remote: InetSocketAddress)(implicit streamEnv: StreamEnv, actorSystem: ActorSystem) = {
     val byteStream = TcpStream.receiver(remote)
+      .buffer(20)
       .via(TcpCheckedLayer.fromChunks2)
+      .bufferDropping(4, OverflowStrategy.DropHead)
     val images = byteStream.asyncBoundary(bufferSize = 0)
       .map(ImageConverter.fromBytes).map { e => println(e); e }
     val window = ImageCanvas.fullscreen(graphicsDevice)
--- src/main/scala/org/lolhens/screencapture/CaptureSender.scala
+++ src/main/scala/org/lolhens/screencapture/CaptureSender.scala
@@ -7,7 +7,7 @@ import akka.actor.ActorSystem
 import cats.implicits._
 import org.lolhens.screencapture.RichSpout._
 import scodec.bits.ByteVector
-import swave.core.StreamEnv
+import swave.core.{Spout, StreamEnv}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -17,32 +17,14 @@ import scala.language.postfixOps
   */
 object CaptureSender {
   def apply(graphicsDevice: GraphicsDevice, remote: InetSocketAddress)(implicit streamEnv: StreamEnv, actorSystem: ActorSystem) = {
-    val grabber0 = ImageGrabber(graphicsDevice)
-    val grabber1 = ImageGrabber(graphicsDevice)
-    val grabber2 = ImageGrabber(graphicsDevice)
-    val grabber3 = ImageGrabber(graphicsDevice)
-    val grabber4 = ImageGrabber(graphicsDevice)
-    val grabber5 = ImageGrabber(graphicsDevice)
-    val grabber6 = ImageGrabber(graphicsDevice)
-    val grabber7 = ImageGrabber(graphicsDevice)
-    val byteStream0 = grabber0.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
-    val byteStream1 = grabber1.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
-    val byteStream2 = grabber2.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
-    val byteStream3 = grabber3.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
-    val byteStream4 = grabber4.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
-    val byteStream5 = grabber5.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
-    val byteStream6 = grabber6.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
-    val byteStream7 = grabber7.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
+    val byteStreams: Seq[Spout[(ByteVector, Long)]] = for (
+      _ <- 0 until Runtime.getRuntime.availableProcessors();
+      grabber = ImageGrabber(graphicsDevice);
+      byteStream = grabber.map(e => (ImageConverter.toBytes2(e._1, "png"), e._2)).asyncBoundary(bufferSize = 0)
+    ) yield byteStream: Spout[(ByteVector, Long)]
 
-    byteStream0
-      .attach(byteStream1)
-      .attach(byteStream2)
-      .attach(byteStream3)
-      .attach(byteStream4)
-      .attach(byteStream5)
-      .attach(byteStream6)
-      .attach(byteStream7)
-      .fanInMerge(eagerComplete = true)
+    Spout.fromIterable(byteStreams)
+      .flattenMerge(2)
       .scanFlatMap[Long, Option, ByteVector](0L) { (last, e) =>
       if (e._2 >= last)
         (e._2, Some(e._1))
